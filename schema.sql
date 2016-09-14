04a39c764c009111342de399c93ca30683eb05ea
skyoo2003 <skyoo2003@gmail.com>
2016-09-14 17:29:53 +0900

    트리거, 외래키 추가

--- schema.sql
+++ schema.sql
@@ -6,18 +6,46 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
 # Dump of table article_images
 # ------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `article_images` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `article_id` bigint(20) unsigned DEFAULT NULL,
+  `article_id` bigint(20) unsigned NOT NULL,
   `image_url` varchar(240) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `article_id` (`article_id`),
   KEY `image_url` (`image_url`),
   CONSTRAINT `fk_article_images_article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DELIMITER ;;
+/*!50003 SET SESSION SQL_MODE="" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`gotcha`@`localhost` */ /*!50003 TRIGGER `insert_article_images_history` AFTER INSERT ON `article_images` FOR EACH ROW BEGIN
+  INSERT INTO article_images_history (article_id, image_url)
+  VALUES (NEW.article_id, NEW.image_url);
+END */;;
+/*!50003 SET SESSION SQL_MODE="" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`gotcha`@`localhost` */ /*!50003 TRIGGER `update_article_images_history` AFTER UPDATE ON `article_images` FOR EACH ROW BEGIN
+  INSERT INTO article_images_history (article_id, image_url)
+  VALUES (NEW.article_id, NEW.image_url);
+END */;;
+DELIMITER ;
+/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
+
+
+# Dump of table article_images_history
+# ------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `article_images_history` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` bigint(20) NOT NULL,
+  `image_url` varchar(240) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -47,7 +75,6 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `writer` varchar(64) NOT NULL DEFAULT '',
   `url` varchar(240) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `category` varchar(32) CHARACTER SET utf8 NOT NULL DEFAULT '',
-  `view_count` int(11) NOT NULL DEFAULT '0',
   `writed_at` datetime NOT NULL,
   `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
@@ -55,6 +82,37 @@ CREATE TABLE IF NOT EXISTS `articles` (
   KEY `category` (`category`),
   KEY `writed_at` (`writed_at`),
   KEY `modified_at` (`modified_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DELIMITER ;;
+/*!50003 SET SESSION SQL_MODE="" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`gotcha`@`localhost` */ /*!50003 TRIGGER `insert_articles_history` AFTER INSERT ON `articles` FOR EACH ROW BEGIN
+  INSERT INTO articles_history (title, content, writer, url, category, writed_at)
+  VALUES (NEW.title, NEW.content, NEW.writer, NEW.url, NEW.category, NEW.writed_at);
+END */;;
+/*!50003 SET SESSION SQL_MODE="" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`gotcha`@`localhost` */ /*!50003 TRIGGER `update_articles_history` AFTER UPDATE ON `articles` FOR EACH ROW BEGIN
+  INSERT INTO articles_history (title, content, writer, url, category, writed_at)
+  VALUES (NEW.title, NEW.content, NEW.writer, NEW.url, NEW.category, NEW.writed_at);
+END */;;
+DELIMITER ;
+/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
+
+
+# Dump of table articles_history
+# ------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `articles_history` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `content` longtext NOT NULL,
+  `writer` varchar(64) NOT NULL DEFAULT '',
+  `url` varchar(240) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `category` varchar(32) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `writed_at` datetime NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -67,7 +125,11 @@ CREATE TABLE IF NOT EXISTS `favorites` (
   `article_id` bigint(20) unsigned NOT NULL,
   `user_id` bigint(20) unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_favorites_article_id` (`article_id`),
+  KEY `fk_favorites_user_id` (`user_id`),
+  CONSTRAINT `fk_favorites_article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_favorites_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -82,7 +144,11 @@ CREATE TABLE IF NOT EXISTS `feedbacks` (
   `emotion` char(1) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_feedbacks_article_id` (`article_id`),
+  KEY `fk_feedbacks_user_id` (`user_id`),
+  CONSTRAINT `fk_feedbacks_article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_feedbacks_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -98,7 +164,11 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   `status` tinyint(4) NOT NULL DEFAULT '1',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_reviews_article_id` (`article_id`),
+  KEY `fk_reviews_user_id` (`user_id`),
+  CONSTRAINT `fk_reviews_article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_reviews_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -120,12 +190,14 @@ CREATE TABLE IF NOT EXISTS `user_groups` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_group_id` bigint(20) unsigned NOT NULL,
+  `user_group_id` bigint(20) unsigned DEFAULT NULL,
   `name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `gender` char(1) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `birthday` date NOT NULL,
   `status` tinyint(4) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_users_user_group_id` (`user_group_id`),
+  CONSTRAINT `fk_users_user_group_id` FOREIGN KEY (`user_group_id`) REFERENCES `user_groups` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
